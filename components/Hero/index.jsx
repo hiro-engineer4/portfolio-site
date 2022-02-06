36de5dc796d97ba6373e6a3613cc19879661e575
cocoa-engineer <thejunkcocoa@gmail.com>
2022-02-07 00:56:47 +0900

    Heroコンポーネントの追加

---
+++ components/Hero/index.jsx
@@ -0,0 +1,18 @@
+export const Hero = () => {
+  return (
+    <div
+      className="flex justify-center items-center h-screen bg-fixed bg-center bg-cover"
+      style={{
+        backgroundImage: "url(" + "/main.jpg" + ")",
+      }}
+    >
+      <div className="flex justify-center items-center w-full h-screen">
+        <div className="mx-4 text-center text-white">
+          <h1 className="mb-4 text-4xl font-bold tracking-widest text-stone-600 md:text-8xl">
+            Cocoa Portfolio
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
