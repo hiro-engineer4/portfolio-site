e7f99756ceb44c45ceebd388874cb6779050cfdb
cocoa-engineer <thejunkcocoa@gmail.com>
2022-02-07 01:07:58 +0900

    Profileコンポーネントの追加

---
+++ components/Profile/index.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+export const Profile = () => {
+  return (
+    <div className="py-32 px-4 text-stone-600 bg-stone-50">
+      <h1 className="mb-20 text-2xl tracking-wider text-center">PROFILE</h1>
+      <div className="md:flex md:px-32">
+        <div className="mb-3 text-center md:pr-4 md:w-1/2 md:text-right">
+          <Image src="/me.jpg" width={240} height={240} alt="profileImage" />
+        </div>
+
+        <div className="flex flex-col justify-center md:w-1/2">
+          <h2 className="mb-3 text-2xl tracking-wide">Hironori Tanimoto</h2>
+          <p className="font-mono tracking-wider leading-6">
+            島根県生まれ。
+            <br />
+            フルリモートでRailsをメインとしたバックエンドエンジニアとして働いています。
+            <br />
+            趣味はFX, 音楽活動。
+            <br />
+            お仕事のご依頼・ご相談等につきましては、Twitterの
+            <a
+              href="https://twitter.com/CocoaEngineer"
+              target="_blank"
+              rel="noreferrer"
+            >
+              DM
+            </a>
+            からお願いします。
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,17 @@
 import { Hero } from "components/Hero";
 import { Navigation } from "components/Navigation";
+import { Profile } from "components/Profile";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Navigation />
+      <main>
+        <section id="profile">
+          <Profile />
+        </section>
+      </main>
     </>
   );
 }
